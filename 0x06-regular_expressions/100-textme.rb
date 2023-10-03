a0aa397e8554bde93816bfc713e52edd57f58838
UmarMsani <umuhammad220@gmail.com>
2023-10-03 03:32:03 -0700

    Regular expression

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -4,9 +4,9 @@
 log_content = ARGF.read
 
 # Extract sender, receiver, and flags
-sender = log_content[/from:([^ \]]+)/, 1]
-receiver = log_content[/to:([^ \]]+)/, 1]
-flags = log_content[/flags:([^ \]]+)/, 1]
+sender = log_content[/from:([^\]]+)/, 1]
+receiver = log_content[/to:([^\]]+)/, 1]
+flags = log_content[/flags:([^\]]+)/, 1]
 
 # Print the formatted output
 puts "#{sender},#{receiver},#{flags}"
