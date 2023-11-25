e06102f3101ca7e95838b0105aef97e79ae414b9
UmarMsani <umuhammad220@gmail.com>
2023-11-25 11:36:20 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,22 +5,5 @@ exec { 'install_flask_2.1.0':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
   path    => '/usr/local/bin:/usr/bin:/bin',
-  require => Package['python3-pip'],
-}
-
-file { '/usr/local/bin/flask_check':
-  ensure  => present,
-  require => Exec['install_flask_2.1.0'],
-}
-
-exec { 'check_flask_version':
-  command     => '/usr/bin/pip3 show Flask | grep "Version: 2.1.0" > /usr/local/bin/flask_check',
-  refreshonly => true,
-  subscribe   => File['/usr/local/bin/flask_check'],
-  notify      => Exec['verify_flask_version'],
-}
-
-exec { 'verify_flask_version':
-  command     => '/usr/local/bin/flask --version',
-  refreshonly => true,
+  require => Package['python3-pip']
 }
