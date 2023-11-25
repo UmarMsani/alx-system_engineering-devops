a44e3dbb6f76bdfdcfe0315440bb2972a96c9ed4
UmarMsani <umuhammad220@gmail.com>
2023-11-25 11:39:28 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# instal Flask version 2.1.0 using pip3
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# installs flask from pip3
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
