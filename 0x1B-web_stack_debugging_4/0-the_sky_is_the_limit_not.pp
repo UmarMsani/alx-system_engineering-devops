a46aa4cf00b22e6ec6ffcef8d16d21774424eced
UmarMsani <umuhammad220@gmail.com>
2024-01-25 14:28:32 -0800

    Web stack debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
-# Increases amount of traffic an Nginx server can handle.
+# Increases amount of traffic an Nginx server can handle
 
-# Increases the ULIMIT of the default file.
+# Increases the ULIMIT of the default file
 exec { 'fix--for-nginx':
 	command => 'sed -i "s/15/4096/" /etc/default/nginx',
 		path    => '/usr/local/bin/:/bin/'
